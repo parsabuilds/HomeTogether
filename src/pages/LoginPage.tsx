bce12517c089098329a9ba5c9681c875cbe15a47
Parsa <114903356+parsabuilds@users.noreply.github.com>
2025-08-01 03:10:11 -0400

    Updated LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -90,9 +90,10 @@ const LoginPage = () => {
             onPasswordFocus={handlePasswordFocus}
             onPasswordBlur={handlePasswordBlur}
           />
-          <span className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+          <h1 className="text-3xl font-bold text-gray-900 mb-2">
+            <span className="text-2xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
                 HomeTogether
-              </span>
+              </span></h1>
         </div>
 
         {/* Tab Navigation */}
